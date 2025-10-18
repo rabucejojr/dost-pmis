70a8801f1414fb625a9b8c36b444fc12adabfa9f
Roger Abucejo Jr <rogerabucejo62@gmail.com>
2025-10-18 21:45:30 +0800

    add role type to user

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -31,6 +31,7 @@ export interface User {
     id: number;
     name: string;
     email: string;
+    roles?: string[];
     avatar?: string;
     email_verified_at: string | null;
     created_at: string;
